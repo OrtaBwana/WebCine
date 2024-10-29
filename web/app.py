e7ba3109398808cc697770a6a70913fb37ef8723
José Rolando <97265804+GRJR1325@users.noreply.github.com>
2024-10-29 21:24:53 +0000

    UPDATE app.py

--- web/app.py
+++ web/app.py
@@ -3,6 +3,7 @@ import requests
 
 API_URL = "https://api-rest-7nlb.onrender.com/peliculas"  
 
+
 # Rutas 
 urls = ("/", "Index")
 
@@ -10,9 +11,9 @@ app = web.application(urls, globals())
 render = web.template.render("web/views/")
 
 class Index:
-
-    def GET(self):
-        try:
+    try:
+        def GET(self):
+            
             response = requests.get(API_URL)
         
             if response.status_code == 200:
@@ -20,12 +21,11 @@ class Index:
                 print("RESPUESTA API:",data)
                 
             else:
-                data = "No hay peliculas"
+                data = [] 
 
             return render.index(data)
+    except Exception as e:
+        print(f"ERROR ENCONTRADO:{e}")
         
-        except Exception as e:
-            print(f"ERROR al insertar la pelicula: {e}")
-
 if __name__ == "__main__":
     app.run()
